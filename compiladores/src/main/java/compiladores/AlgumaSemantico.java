95f5169f42a052d95fe7a36ca28cad6bf44285d6
IvanCalvo <ivandc1998.ic@gmail.com>
2023-07-07 09:56:38 -0300

    inicio AlgumaSemantico.java

--- compiladores/src/main/java/compiladores/AlgumaSemantico.java
+++ compiladores/src/main/java/compiladores/AlgumaSemantico.java
@@ -1,5 +1,14 @@
 package compiladores;
 
-public class AlgumaSemantico {
-    
+import compiladores.AlgumaParser.Declaracao_globalContext;
+import compiladores.AlgumaParser.Declaracao_localContext;
+import compiladores.AlgumaParser.DeclaracoesContext;
+import compiladores.AlgumaParser.ProgramaContext;
+import compiladores.AlgumaParser.IdentificadorContext;
+import compiladores.AlgumaParser.CmdAtribuicaoContext;
+import compiladores.AlgumaParser.Tipo_basico_identContext;
+import compiladores.TabelaDeSimbolos.TipoAlguma;
+
+public class AlgumaSemantico extends AlgumaBaseVisitor<Void> {
+
 }
