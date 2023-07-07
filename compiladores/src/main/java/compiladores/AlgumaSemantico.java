7df1a6f5a379e9e084ad44b5800288fe2cb1f384
IvanCalvo <ivandc1998.ic@gmail.com>
2023-07-07 11:31:41 -0300

    semantico ainda não

--- compiladores/src/main/java/compiladores/AlgumaSemantico.java
+++ compiladores/src/main/java/compiladores/AlgumaSemantico.java
@@ -10,5 +10,42 @@ import compiladores.AlgumaParser.Tipo_basico_identContext;
 import compiladores.TabelaDeSimbolos.TipoAlguma;
 
 public class AlgumaSemantico extends AlgumaBaseVisitor<Void> {
+    TabelaDeSimbolos tabela;
+
+    @Override
+    public Void visitPrograma(AlgumaParser.ProgramaContext ctx) {
+        tabela = new TabelaDeSimbolos();
+        return super.visitPrograma(ctx);
+    }
+
+    @Override
+    public Void visitDeclaracao_local(AlgumaParser.Declaracao_localContext ctx){
+        String nomeVar = ctx.variavel().getText();
+        String strTipoVar = ctx.tipo().getText();
+        TipoAlguma tipoVar = TipoAlguma.INVALIDO;
+        switch (strTipoVar) {
+            case "INTEIRO":
+                tipoVar = TipoAlguma.INTEIRO;
+                break;
+            case "REAL":
+                tipoVar = TipoAlguma.REAL;
+                break;
+            default:
+                // Nunca irá acontecer, pois o analisador sintático
+                // não permite
+                break;
+        }
+
+        // Verificar se a variável já foi declarada
+        if (tabela.existe(nomeVar)) {
+            AlgumaSemanticoUtils.adicionarErroSemantico(ctx.variavel().getSymbol(), "Variável " + nomeVar + " já existe");
+        } else {
+            tabela.adicionar(nomeVar, tipoVar);
+        }
+
+        return super.visitDeclaracao_local(ctx);
+
+    }
+
 
 }
