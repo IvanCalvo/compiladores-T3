a222f9bfc283d547d5a9f3d15740e6cbbe465e3a
IvanCalvo <ivandc1998.ic@gmail.com>
2023-07-07 08:31:29 -0300

    consegui mudar o nome finalmente

--- compiladores/src/main/java/compiladores/App.java
+++ compiladores/src/main/java/compiladores/App.java
@@ -1,20 +1,36 @@
 package compiladores;
-
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import compiladores.AlgumaParser;
+
+import compiladores.AlgumaParser.ProgramaContext;
 
 public class App {
 
-    public static void main(String args[]) throws IOException {
-        CharStream cs = CharStreams.fromFileName(args[0]);
-        /*ExpressoesLexer lexer = new ExpressoesLexer(cs);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ExpressoesParser parser = new ExpressoesParser(tokens);
-        int val = parser.programa().val;
-        System.out.println("Valor = " + val);
-        */
-
+    public static void main(String args[]) {
+        try(PrintWriter p = new PrintWriter(new File(args[1]))) {//saida
+            CharStream c = CharStreams.fromFileName(args[0]);//entrada
+            AlgumaLexer lex = new AlgumaLexer(c);
+            CommonTokenStream cs = new CommonTokenStream(lex); //conversão para token stream
+            AlgumaParser parser = new AlgumaParser(cs);
+            AlgumaParser.ProgramaContext arvore = parser.programa();   
+            /*AlgumaSemantico as = new AlgumaSemantico();  
+            as.visitPrograma(arvore);
+            for(String err: SemanticoUtils.errosSemanticos){
+                p.println(err);
+            }*/
+            p.println("Fim da compilacao");
+            p.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
     }
 }
