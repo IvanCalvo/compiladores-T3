2290fec02ee9578d0686012583c1b505c00cc75f
vini <viniciusborges@estudante.ufscar.br>
2023-07-09 21:47:54 -0300

    Classe erro atualizada

--- compiladores/src/main/java/compiladores/MensagemErro.java
+++ compiladores/src/main/java/compiladores/MensagemErro.java
@@ -2,27 +2,65 @@ package compiladores;
 
 import compiladores.AlgumaLexer;
 import compiladores.AlgumaParser;
+
+import java.io.PrintWriter;
+
 import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.ANTLRErrorStrategy;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Token;
+import java.util.BitSet;
+
+
 
 public class MensagemErro implements ANTLRErrorListener {
-    @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int column, String msg,
-            RecognitionException e) {
-        String erro = recognizer.getInputStream()
-                .getText(new Interval(e.getOffendingToken().getStartIndex(), e.getOffendingToken().getStopIndex()));
+    public PrintWriter p;
 
-        if (erro.equals("\"")) {
-            throw new RuntimeException("Linha " + line + ": cadeia literal nao fechada");
-        } else if (erro.equals("{")) {
-            throw new RuntimeException("Linha " + line + ": comentario nao fechado");
-        } else {
-            throw new RuntimeException("Linha " + line + ": " + erro + " - simbolo nao identificado");
+    static boolean got_error = false; // utilizado para evitar impressões após encontrar primeiro erro
+    public MensagemErro(PrintWriter p){
+        this.p = p;
+    }
+
+   
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> arg0, Object arg1, int arg2, int arg3, String arg4,
+            RecognitionException arg5) {
+        
+        // TODO Auto-generated method stub
+        // Conversão do token stream para token unico
+        Token t = (Token) arg1;
+        String text = t.getText();
+        // o texto de EOF vem com <>, portanto aqui realiza uma conversão em tal caso
+        if(text == "<EOF>")
+            text = "EOF";
+        String aType = AlgumaLexer.VOCABULARY.getDisplayName(t.getType()); // Converte o tipo desse token para string
+        if(!got_error){
+
+            if(aType == "Nao_Fechado"){ //Caso seja comentario nao fechado
+                p.println("Linha " + t.getLine() + ": " + "comentario nao fechado");
+                MensagemErro.got_error = true;
+            }
+            else if(aType == "Literal_Nao_Fechada"){ //Caso seja literal "" nao fechado
+                p.println("Linha " + t.getLine() + ": " + "cadeia literal nao fechada");
+                MensagemErro.got_error = true;
+            }
+            else if(aType == "ERR"){ //Em caso de Simbolo nao identificado
+                p.println("Linha " + t.getLine() + ": " + text + " - simbolo nao identificado");
+                MensagemErro.got_error = true;
+            }
+            else{
+                p.println("Linha " + arg2 + ": erro sintatico proximo a " + text);
+                MensagemErro.got_error = true;
+            }
         }
+
+        
     }
 
     @Override
