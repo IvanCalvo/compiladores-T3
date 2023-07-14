995e6678847a0b256d51b296dbce6c564e73e003
vini <viniciusborges@estudante.ufscar.br>
2023-07-14 09:01:12 -0300

    Comentários

--- compiladores/src/main/java/compiladores/AlgumaSemanticoUtils.java
+++ compiladores/src/main/java/compiladores/AlgumaSemanticoUtils.java
@@ -14,17 +14,24 @@ import compiladores.AlgumaParser.TermoContext;
 import compiladores.AlgumaParser.Termo_logicoContext;
 
 public class AlgumaSemanticoUtils {
+    // Lista que armazena mensagens de erros semânticos
     public static List<String> errosSemanticos = new ArrayList<>();
-    
+
+    // Método para adicionar uma mensagem de erro semântico à lista
     public static void adicionarErroSemantico(Token t, String mensagem) {
-        int linha = t.getLine();    
+        int linha = t.getLine();
+        // Formata a mensagem de erro com a linha do token e a mensagem fornecida
         errosSemanticos.add(String.format("Linha %d: %s", linha, mensagem));
     }
-    
+
+    // Método que realiza a verificação semântica de uma expressão
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.ExpressaoContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
+        // Percorre todos os termos lógicos presentes na expressão
         for (Termo_logicoContext ta : ctx.termo_logico()) {
+            // Realiza a verificação semântica do termo lógico
             TabelaDeSimbolos.TipoAlguma aux = verificar(escopos, ta);
+            // Atualiza o tipo de retorno da expressão de acordo com o tipo do termo lógico
             if (ret == null) {
                 ret = aux;
             } else if (ret != aux && aux != TabelaDeSimbolos.TipoAlguma.INVALIDO) {
@@ -34,10 +41,14 @@ public class AlgumaSemanticoUtils {
         return ret;
     }
 
+    // Método que realiza a verificação semântica de um termo lógico
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.Termo_logicoContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
+        // Percorre todos os fatores lógicos presentes no termo lógico
         for (Fator_logicoContext ta : ctx.fator_logico()) {
+            // Realiza a verificação semântica do fator lógico
             TabelaDeSimbolos.TipoAlguma aux = verificar(escopos, ta);
+            // Atualiza o tipo de retorno do termo lógico de acordo com o tipo do fator lógico
             if (ret == null) {
                 ret = aux;
             } else if (ret != aux && aux != TabelaDeSimbolos.TipoAlguma.INVALIDO) {
@@ -47,66 +58,80 @@ public class AlgumaSemanticoUtils {
 
         return ret;
     }
-
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.Fator_logicoContext ctx) {
+        // Verifica o fator lógico chamando o método verificar para a parcela lógica
         return verificar(escopos, ctx.parcela_logica());
     }
-
+    
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.Parcela_logicaContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
-        if(ctx.exp_relacional() != null){
+        // Verifica se a parcela lógica contém uma expressão relacional
+        if (ctx.exp_relacional() != null) {
+            // Verifica a expressão relacional
             ret = verificar(escopos, ctx.exp_relacional());
-        } else{
+        } else {
+            // Caso contrário, o tipo de retorno é lógico
             ret = TabelaDeSimbolos.TipoAlguma.LOGICO;
         }
-
+    
         return ret;
     }
-
+    
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.Exp_relacionalContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
-        if(ctx.op_relacional() != null){
+        // Verifica se a expressão relacional possui um operador relacional
+        if (ctx.op_relacional() != null) {
+            // Percorre as expressões aritméticas presentes na expressão relacional
             for (Exp_aritmeticaContext ta : ctx.exp_aritmetica()) {
+                // Verifica a expressão aritmética
                 TabelaDeSimbolos.TipoAlguma aux = verificar(escopos, ta);
                 Boolean auxNumeric = aux == TabelaDeSimbolos.TipoAlguma.REAL || aux == TabelaDeSimbolos.TipoAlguma.INTEIRO;
                 Boolean retNumeric = ret == TabelaDeSimbolos.TipoAlguma.REAL || ret == TabelaDeSimbolos.TipoAlguma.INTEIRO;
+                // Atualiza o tipo de retorno da expressão relacional de acordo com o tipo da expressão aritmética
                 if (ret == null) {
                     ret = aux;
                 } else if (!(auxNumeric && retNumeric) && aux != ret) {
                     ret = TabelaDeSimbolos.TipoAlguma.INVALIDO;
                 }
             }
-            if(ret != TabelaDeSimbolos.TipoAlguma.INVALIDO){
+            // Se o tipo de retorno da expressão relacional não for inválido, ele é do tipo lógico
+            if (ret != TabelaDeSimbolos.TipoAlguma.INVALIDO) {
                 ret = TabelaDeSimbolos.TipoAlguma.LOGICO;
             }
         } else {
+            // Caso contrário, a expressão relacional é reduzida para uma expressão aritmética
             ret = verificar(escopos, ctx.exp_aritmetica(0));
         }
-
+    
         return ret;
     }
-
+    
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.Exp_aritmeticaContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
+        // Percorre os termos presentes na expressão aritmética
         for (TermoContext ta : ctx.termo()) {
+            // Verifica o termo
             TabelaDeSimbolos.TipoAlguma aux = verificar(escopos, ta);
+            // Atualiza o tipo de retorno da expressão aritmética de acordo com o tipo do termo
             if (ret == null) {
                 ret = aux;
             } else if (ret != aux && aux != TabelaDeSimbolos.TipoAlguma.INVALIDO) {
                 ret = TabelaDeSimbolos.TipoAlguma.INVALIDO;
             }
         }
-
+    
         return ret;
     }
-
+    
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.TermoContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
-
+        // Percorre os fatores presentes no termo
         for (FatorContext fa : ctx.fator()) {
+            // Verifica o fator
             TabelaDeSimbolos.TipoAlguma aux = verificar(escopos, fa);
             Boolean auxNumeric = aux == TabelaDeSimbolos.TipoAlguma.REAL || aux == TabelaDeSimbolos.TipoAlguma.INTEIRO;
             Boolean retNumeric = ret == TabelaDeSimbolos.TipoAlguma.REAL || ret == TabelaDeSimbolos.TipoAlguma.INTEIRO;
+            // Atualiza o tipo de retorno do termo de acordo com o tipo do fator
             if (ret == null) {
                 ret = aux;
             } else if (!(auxNumeric && retNumeric) && aux != ret) {
@@ -115,6 +140,7 @@ public class AlgumaSemanticoUtils {
         }
         return ret;
     }
+    
     public static TabelaDeSimbolos.TipoAlguma verificar(Escopos escopos, AlgumaParser.FatorContext ctx) {
         TabelaDeSimbolos.TipoAlguma ret = null;
 
